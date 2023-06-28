228855a8766d3cbee01cda02e24219837fcdd4d4
MatiasSchultz <matiisch1@gmail.com>
2023-06-27 21:54:06 -0300

    c

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(indexRoutes);
 
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/login"); // Reemplaza con la URL de tu proyecto
+	res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/login");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
